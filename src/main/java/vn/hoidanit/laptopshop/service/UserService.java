e8b3f210a165ba9707fc4d111c1eb0795b6e939d
Nguyen Duy Minh Nguyen <nguyenduyminh300kg@gmail.com>
2024-09-24 23:14:50 +0700

    sua bien eric -> ericNotHTML trong hello.jsp

--- src/main/java/vn/hoidanit/laptopshop/service/UserService.java
+++ src/main/java/vn/hoidanit/laptopshop/service/UserService.java
@@ -5,6 +5,6 @@ import org.springframework.stereotype.Service;
 @Service
 public class UserService {
     public String handleHello() {
-        return "Hello from Service";
+        return "Hello from Service ";
     }
 }
